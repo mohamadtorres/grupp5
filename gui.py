700898a73a60b663e2d2d62a774f8309af6c4f0c
aerngren <adam_erngren@hotmail.com>
2024-12-12 08:52:32 +0100

    Gjort om så man alltid gör en kvadratisk karta

--- gui.py
+++ gui.py
@@ -47,15 +47,12 @@ def start():
 
 def main_menu():
     user_input_x = ""
-    user_input_y = ""
 
     input_rect_x = pygame.Rect(200, 150, 140,32)
-    input_rect_y = pygame.Rect(200, 200, 140,32)
     color_passive = 90, 112, 107
     color_active = 102, 87, 87
     active_x = False
-    active_y = False
-    start_button = Button(200, 250, start_img, 0.25)
+    start_button = Button(200, 200, start_img, 0.25)
     pygame.display.set_caption('Menu')
     while True:
         for event in pygame.event.get():
@@ -65,17 +62,13 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:                
                 if input_rect_x.collidepoint(event.pos):
                     active_x = True
-                    active_y = False
-                elif input_rect_y.collidepoint(event.pos):
-                    active_y = True
-                    active_x = False
                 elif start_button.rect.collidepoint(event.pos):
-                    kart_generator(int(user_input_x), int(user_input_y))
-                    #start()
+                    if 9 < int(user_input_x) < 201:
+                        kart_generator(int(user_input_x), int(user_input_x))
+                    else:
+                        print("Fuck")
                 else:
-                    active_x = False
-                    active_y = False
-                
+                    active_x = False                
 
             if event.type == pygame.KEYDOWN:
                 if active_x == True:
@@ -87,34 +80,19 @@ def main_menu():
                             user_input_x += event.unicode
                         elif event.unicode.isdigit():
                             user_input_x += event.unicode
-                if active_y == True:
-                    if event.key == pygame.K_BACKSPACE:
-                        user_input_y = user_input_y[:-1]
-                    else:
-                        if len(user_input_y) > 2 and event.unicode.isdigit():
-                            user_input_y = user_input_y[1:]
-                            user_input_y += event.unicode
-                        elif event.unicode.isdigit():
-                            user_input_y += event.unicode
+
         if active_x:
             color_x = color_active
-            color_y = color_passive
-        elif active_y:
-            color_y = color_active
-            color_x = color_passive
         else:
             color_x = color_passive 
-            color_y = color_passive
         SCREEN.blit(BG, (0,0))
-        draw_text("THE SUB MAZE", font, TEXT_COL, 200, 50)
+        draw_text("THE SUB MAZE", font, TEXT_COL, 160, 50)
 
         pygame.draw.rect(SCREEN,color_x, input_rect_x)
-        pygame.draw.rect(SCREEN,color_y, input_rect_y)
 
         text_surface_x = input_font.render(user_input_x, True, TEXT_COL)
-        text_surface_y = input_font.render(user_input_y, True, TEXT_COL)
+
         SCREEN.blit(text_surface_x, (input_rect_x.x+5, input_rect_x.y))
-        SCREEN.blit(text_surface_y, (input_rect_y.x+5, input_rect_y.y))
         
         start_button.draw()
         pygame.display.update()
